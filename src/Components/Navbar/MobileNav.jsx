5cafef0a373e05f7a4cec0e7684208e141977d57
Hasibul Hasan Rokib <134086135+HasibulHasanRokib@users.noreply.github.com>
2023-07-27 18:21:11 +0600

    Initial commit

--- src/Components/Navbar/MobileNav.jsx
+++ src/Components/Navbar/MobileNav.jsx
@@ -1,18 +1,20 @@
 import './MobileNav.css'
 import { useContext} from "react";
 import ProductsContext from "../Context/ProductsContext";
+import { useNavigate } from 'react-router-dom';
 
 const MobileNav = () => {
 
   const{cartShow,setCartShow,cartItems,wishShow,setWishShow,wishItems }=useContext(ProductsContext)
 
+  const navigate=useNavigate()
 
   return (
     <div>
      <div className="mobile-nav">
       <ul className='mobile-nav-items'>
         <li>
-          <button><i className="fa-solid fa-house text-2xl"></i></button>
+          <button onClick={()=>navigate('/')}><i className="fa-solid fa-house text-2xl"></i></button>
           </li>
           <li>
           <button onClick={()=>setCartShow(!cartShow)}  className='flex items-center'><i className="fa-solid fa-cart-shopping text-2xl"></i><span>({cartItems.length})</span></button>
