650022aedd4085f7a490f4a4c404c9c71a3daa92
Hasibul Hasan Rokib <134086135+HasibulHasanRokib@users.noreply.github.com>
2023-07-29 22:23:10 +0600

    Initial commit

--- src/Components/Navbar/MobileNav.jsx
+++ src/Components/Navbar/MobileNav.jsx
@@ -14,26 +14,26 @@ const MobileNav = () => {
      <div className="mobile-nav">
       <ul className='mobile-nav-items'>
       <li className='flex justify-center items-center flex-col'>
-          <button onClick={()=>setCategoryShow(!categoryShow)}><i className="fa-solid fa-bars text-2xl"></i></button>
-          <p className='text-xs'>Category</p>
+          <button style={{color:"var(--blue)"}} onClick={()=>setCategoryShow(!categoryShow)}><i className="fa-solid fa-bars text-2xl"></i></button>
+          <p style={{color:"var(--blue)"}} className='text-xs font-bold'>Category</p>
         </li>
           <li>
-          <button onClick={()=>setCartShow(!cartShow)}  className='flex items-center'><i className="fa-solid fa-cart-shopping text-2xl"></i><span>({cartItems.length})</span></button>
-          <p className='text-xs'>Cart</p>
+          <button style={{color:"var(--blue)"}} onClick={()=>setCartShow(!cartShow)}><i className="fa-solid fa-cart-shopping text-2xl"></i><span>({cartItems.length})</span></button>
+          <p style={{color:"var(--blue)"}} className='text-xs font-bold'>Cart</p>
 
           </li>
           <li>
-          <button onClick={()=>navigate('/')}><i className="fa-solid fa-house text-2xl home-btn"></i></button>
+          <button style={{color:"var(--blue)"}} onClick={()=>navigate('/')}><i className="fa-solid fa-house text-2xl home-btn"></i></button>
           </li>
           <li>
-          <button onClick={()=>setWishShow(!wishShow)}  className='flex items-center'><i className="fa-solid fa-heart text-2xl"></i><span>({wishItems.length})</span></button>
-          <p className='text-xs'>Wishlist</p>
+          <button style={{color:"var(--blue)"}} onClick={()=>setWishShow(!wishShow)} ><i className="fa-solid fa-heart text-2xl"></i><span>({wishItems.length})</span></button>
+          <p style={{color:"var(--blue)"}} className='text-xs font-bold'>Wishlist</p>
 
           </li>
      
         <li>
-          <button onClick={()=>navigate('signUp')}><i className="fa-solid fa-user text-2xl"></i></button>
-          <p className='text-xs'>User</p>
+          <button style={{color:"var(--blue)"}} onClick={()=>navigate('signUp')}><i className="fa-solid fa-user text-2xl "></i></button>
+          <p style={{color:"var(--blue)"}} className='text-xs font-bold'>User</p>
 
           </li>
       </ul>
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -9,9 +9,9 @@ const SignUp = () => {
       </section>
 
         <div className='signUp-img'>
-            <img className='mt-16' src="https://skybuybd.com/_next/static/media/login_icon.849dedcc.svg" alt="" />
+            <img src="https://skybuybd.com/_next/static/media/login_icon.849dedcc.svg" alt="" />
         </div>
-        <form className='flex flex-col justify-center items-center mt-16  p-5'>
+        <form className='flex flex-col justify-center items-center md:mt-15 md:p-5'>
         <h2>এখানে আপনার মোবাইল নাম্বার দিয়ে লগিন করুন</h2>
         <span className='flex flex-col justify-center items-start my-10'>
         <label>মোবাইল নাম্বার</label>
